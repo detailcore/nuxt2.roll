f5d3edb1643251d7b08f677936675196466583ff
savemysoulok@yandex.ru <admin@server.example.com>
2024-01-15 20:39:18 +0300

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps: [
     {
       name: "Nuxt2Roll",
+      port: "3007",
       // exec_mode: 'cluster',
       // instances: 3, // 'max' or a number of instances
       script: "./node_modules/nuxt/bin/nuxt.js",
